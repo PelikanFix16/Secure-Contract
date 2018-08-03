30ba63a1e054888106c644c00071465940334c82
Hubert Osypowicz <hubertosypowicz@outlook.com>
2018-08-03 17:57:38 +0200

    Working Contract Creator (modified genesis file geth added byzantiumBlock)

--- src/Core/Contracts/migrations/2_deploy_contracts.js
+++ src/Core/Contracts/migrations/2_deploy_contracts.js
@@ -4,6 +4,7 @@ var TradeCenter = artifacts.require("./TradeCenter.sol");
 
 module.exports = function(deployer) {
 
-deployer.deploy(SafeCoin).then(function(){
-    deployer.deploy(TradeCenter, SafeCoin.address)});
+    deployer.deploy(SafeCoin).then(function(){
+        deployer.deploy(TradeCenter, SafeCoin.address)});
+
 };
